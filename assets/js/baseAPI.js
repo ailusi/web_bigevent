cbf14d01a3ebe78b8a3a953351beb2be835eee6d
mik <1284505150@qq.com>
2022-04-28 19:03:51 +0800

    主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,22 @@
 
 $.ajaxPrefilter(function(options){
 options.url='http://www.liulongbin.top:3007'+options.url
+// 权限接口
+if(options.url.indexOf('/my/')!==-1){
+    options.headers={
+         Authorization: localStorage.getItem('token')||''
+        }
+}
+
+// 全局统一挂载complete回调函数
+options.complete=function (res) {
+    // console.log(res);
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //强制情况token
+        localStorage.removeItem('token')
+        // 强制跳转登录页面
+        location.href='/login.html'
+    }
+} 
 })
+
